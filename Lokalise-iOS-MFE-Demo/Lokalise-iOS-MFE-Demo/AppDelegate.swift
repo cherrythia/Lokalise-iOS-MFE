9a6d7d2c8937439a061ba38eb609c2042a1835d0
Terry Chia <chiawz@hotmail.com>
2023-10-20 16:53:53 +0800

    chore: testing updated by using workaround

--- Lokalise-iOS-MFE-Demo/Lokalise-iOS-MFE-Demo/AppDelegate.swift
+++ Lokalise-iOS-MFE-Demo/Lokalise-iOS-MFE-Demo/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Lokalise_iOS_MFE
+import Lokalise
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let ota = OTA()
-        ota.StartOTA()
+//        let ota = OTA()
+//        ota.StartOTA()
+        setupOTALokalise()
         
         return true
     }
@@ -33,6 +35,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    private func setupOTALokalise() {
+        Lokalise.shared.setProjectID("455009485e1e85825682d2.64484346", token: "")
+        
+        Lokalise.shared.swizzleMainBundle()
+        
+        Lokalise.shared.localizationType = .prerelease
+        
+        Lokalise.shared.checkForUpdates { (updated, errorOrNil) in
+            print("Updated: \(updated)\nError: \(errorOrNil)")
+            
+            print("""
+                        Locale
+                        
+                        Locale.preferredLanguages
+                        \(Locale.preferredLanguages)
+                        Locale.current.identifier
+                        \(Locale.current.identifier)
+                        
+                        
+                        Bundle
+                        
+                        Bundle.main.localizations
+                        \(Bundle.main.localizations)
+                        Bundle.main.preferredLocalizations
+                        \(Bundle.main.preferredLocalizations)
+                        Bundle.main.developmentLocalization
+                        \(Bundle.main.developmentLocalization ?? "undefined")
+                        
+                        
+                        Lokalise
+                        
+                        Lokalise.shared.availableLocales .identifier
+                        \(Lokalise.shared.availableLocales().map({$0.identifier}))
+                        Lokalise.shared.localizationLocale.identifier
+                        \(Lokalise.shared.localizationLocale.identifier)
+                        Lokalise.shared.lokaliseBundleVersion
+                        \(Lokalise.shared.lokaliseBundleVersion)
+                        LokaliseFrameworkVersion
+                        \(LokaliseFrameworkVersion)
+                        """)
+            
+            
+        }
     }
 
 
--- Lokalise-iOS-MFE/Lokalise-iOS-MFE/LocalizableStrings.swift
+++ Lokalise-iOS-MFE/Lokalise-iOS-MFE/LocalizableStrings.swift
@@ -6,15 +6,29 @@
 //
 
 import Foundation
+import Lokalise
 
 enum LocalizableStrings: String {
+
     case otaKey = "ota_key"
+    case something = "something"
     
     var localized: String {
         guard let bundle = Bundle(identifier: "com.test.Lokalise-iOS-MFE") else {
             return self.rawValue
         }
         
-        return NSLocalizedString(self.rawValue, bundle: bundle, comment: "")
+//        let local = NSLocalizedString(self.rawValue, bundle: bundle, comment: "")
+//        return Lokalise.shared.localizedString(forKey: self.rawValue, value: local, table: "Localizable")
+        return LokaliseLocalizedString(key: self.rawValue, value: nil, table: "Localizable", fallbackBundle: bundle)
+    }
+    
+    func LokaliseLocalizedString(key: String, value: String?, table: String?, fallbackBundle: Bundle) -> String {
+        let remoteValue = Lokalise.shared.localizedString(forKey: key, value: value, table: table)
+        if (remoteValue == value) {
+            return fallbackBundle.localizedString(forKey: key, value: value, table: table)
+        } else {
+            return remoteValue
+        }
     }
 }
